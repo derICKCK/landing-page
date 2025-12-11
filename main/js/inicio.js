8824a2f1f032e7c5de134069282081e238e34500
Derick <derick@gmail.com>
2025-12-11 09:48:29 +0100

    a lot of changes xd

--- main/js/inicio.js
+++ main/js/inicio.js
@@ -1,12 +1,29 @@
-const elementos = document.querySelectorAll(".reveal");
+const elementos = document.querySelectorAll(".oculto");
 
 function mostrar() {
-  elementos.forEach(el => {
-    const top = el.getBoundingClientRect().top;
-    if (top < window.innerHeight - 100) {
-      el.classList.add("visible");
+  elementos.forEach(elem => {
+    const top = elem.getBoundingClientRect().top;
+    if (top < window.innerHeight -100) {
+      elem.classList.add("visible");
     }
   });
 }
 
 window.addEventListener("scroll", mostrar);
+
+
+function irAlInicio() {
+    window.location.href = "index.html"; 
+}
+
+document.getElementById("milogo").addEventListener("click", irAlInicio);
+
+
+
+
+function iralprincipio() {
+    window.location.href = "index.html"; 
+}
+
+document.getElementById("milogoo").addEventListener("click", iralprincipio);
+
