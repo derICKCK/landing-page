1d3c5c02a908c44808ff93e487cca46328072634
Derick <derick@gmail.com>
2025-12-15 10:55:42 +0100

    aura

--- main/js/inicio.js
+++ main/js/inicio.js
@@ -1,17 +1,27 @@
+/**Funcion para ocultar los contendios del cuerpo hasta que se deslice abajo */
 const elementos = document.querySelectorAll(".oculto");
-
+//funcion1
 function mostrar() {
+  const elementos = document.querySelectorAll(".oculto"); // se ejecuta cada vez que haces scroll
   elementos.forEach(elem => {
     const top = elem.getBoundingClientRect().top;
-    if (top < window.innerHeight -100) {
+    if (top < window.innerHeight - 100) {
       elem.classList.add("visible");
     }
   });
 }
 
-window.addEventListener("scroll", mostrar);
+//Boton para empezar
+document.getElementById("boton").addEventListener("click", () => {
+  document.getElementById("redireccion").scrollIntoView({
+    behavior: "smooth"
+  });
+});
 
 
+
+/**Funcion para ir al inicio al hacer click en el logo */
+//Funcion 2
 function irAlInicio() {
     window.location.href = "index.html"; 
 }
@@ -19,11 +29,57 @@ function irAlInicio() {
 document.getElementById("milogo").addEventListener("click", irAlInicio);
 
 
-
+/*Funcion para ir al inicio al hacer click en el logo pero del footer 
+funcion 3*/
 
 function iralprincipio() {
     window.location.href = "index.html"; 
 }
 
 document.getElementById("milogoo").addEventListener("click", iralprincipio);
+// Array
+const servicios = [
+  {
+    titulo: "Equipo",
+    descripcion: "Grabación con dispositivo móvil adaptados a tu estilo."
+  },
+  {
+    titulo: "Programas de Edición",
+    descripcion: "Edición enfocada en impacto visual y viralidad."
+  },
+  {
+    //
+    titulo: "Gestion de redes Social",
+    //Usamos un array para listar los servicios de descripcion y luego usar un join
+    descripcion: ["Optimización para TikTok","Reels","Shorts"]
+  }
+];
+  //<Seleccionar de html un id
+const contenedorServicios = document.getElementById("servicios");
+  /*Funcion para mostrar los servicios en cartas
 
+  Funcion 4*/
+
+function mostrarServicios() {
+  servicios.forEach(servicio => {
+    const card = document.createElement("div");
+    card.classList.add("card", "oculto");
+
+    card.innerHTML = `
+      <h3>${servicio.titulo}</h3>
+      <p>${
+        Array.isArray(servicio.descripcion)
+          ? servicio.descripcion.join(", ")
+          : servicio.descripcion
+      }</p>
+    `;
+
+    contenedorServicios.appendChild(card);
+  });
+}
+// Crear cards
+mostrarServicios(); 
+// Mostrar las que ya están en pantalla 
+mostrar();          
+ // Animación al hacer scroll 
+window.addEventListener("scroll", mostrar);
---
+++ main/js/sobremi.js
@@ -0,0 +1,15 @@
+/**Funcion para ir al inicio al hacer click en el logo */
+//Funcion 1
+function irAlInicio() {
+    window.location.href = "index.html"; 
+}
+
+document.getElementById("milogo").addEventListener("click", irAlInicio);
+
+/**Funcion para ir al inicio al hacer click en el logo footer*/
+//Funcion 2
+function iralprincipio() {
+    window.location.href = "index.html"; 
+}
+
+document.getElementById("milogoo").addEventListener("click", iralprincipio);
